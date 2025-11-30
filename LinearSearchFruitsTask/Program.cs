5f3876005bba3a50253b8eb7ba2a2e435a6b90f0
Reva Tandale <22RTandale@heckgrammar.co.uk>
2025-11-30 15:34:42 +0000

    a

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -10,9 +10,21 @@
             // Ask the user what word they would like to find
             // Output the message True if the word is found
             // Output the message False if the word is not found
-            
+
             // You must write your own linear search routine and not use any built-in fuctions available in C#
             // You should use meaningful variable names
+            Console.WriteLine("What word would you like to find?");
+            string SearchingFor = Console.ReadLine();
+            bool found = false;
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                if (SearchingFor == fruits[i])
+                {
+                    found = true;
+                    break;
+                }
+            }
+            Console.WriteLine(found);
         }
     }
 }
